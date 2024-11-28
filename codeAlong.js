f45ea61b2d2d77d0a4b90fd100719bb10aff834c
Mandus Lindström <mandus85@hotmail.com>
2024-11-28 10:05:40 +0100

    start of code along async/await

--- codeAlong.js
+++ codeAlong.js
@@ -133,59 +133,59 @@
 
 // Steg 2: Definiera Funktioner för att Hämta Data
 // Vi kommer att hämta data för flera användare och deras inlägg parallellt.
-function getUser(userId) {
-  return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`).then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error("Kunde inte hämta användare med ID:et: ", userId);
-      }
-      return response.json();
-    }
-  );
-}
+// function getUser(userId) {
+//   return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`).then(
+//     (response) => {
+//       if (!response.ok) {
+//         throw new Error("Kunde inte hämta användare med ID:et: ", userId);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
 
-function getPosts(userId) {
-  return fetch(
-    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(
-        "Kunde inte hämta inlägg för användare med ID:et: ",
-        userId
-      );
-    }
-    return response.json();
-  });
-}
+// function getPosts(userId) {
+//   return fetch(
+//     `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+//   ).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(
+//         "Kunde inte hämta inlägg för användare med ID:et: ",
+//         userId
+//       );
+//     }
+//     return response.json();
+//   });
+// }
 
 // Steg 3: Använda Promise.all() för Parallella Hämtningar
 // Vi ska nu hämta data för flera användare samtidigt.
-const userIds = [1, 2, 3];
+// const userIds = [1, 2, 3];
 
-const userPromises = userIds.map((id) => getUser(id));
-const postPromises = userIds.map((id) => getPosts(id));
+// const userPromises = userIds.map((id) => getUser(id));
+// const postPromises = userIds.map((id) => getPosts(id));
 
 // Modifierad Kod med Korrekt Referens till users
-let userData;
+// let userData;
 
-Promise.all(userPromises)
-  .then((users) => {
-    userData = users;
-    console.log("Användare hämtade: ", users);
-    return Promise.all(postPromises);
-  })
-  .then((postsArr) => {
-    console.log("Inlägg hämtade: ", postsArr);
-    postsArr.forEach((post, index) => {
-      console.log(`Inlägg för ${userData[index].name}`);
-      post.forEach((post) => {
-        console.log(post.title);
-      });
-    });
-  })
-  .catch((err) => {
-    console.error("Ett fel uppstod: ", err);
-  });
+// Promise.all(userPromises)
+//   .then((users) => {
+//     userData = users;
+//     console.log("Användare hämtade: ", users);
+//     return Promise.all(postPromises);
+//   })
+//   .then((postsArr) => {
+//     console.log("Inlägg hämtade: ", postsArr);
+//     postsArr.forEach((post, index) => {
+//       console.log(`Inlägg för ${userData[index].name}`);
+//       post.forEach((post) => {
+//         console.log(post.title);
+//       });
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Ett fel uppstod: ", err);
+//   });
 
 //   -----------------------------------------------------
 //   -----------------------------------------------------
@@ -202,10 +202,62 @@ Promise.all(userPromises)
 // Eftersom Promise.all() reject:ar om någon av Promises reject:ar, kan vi fånga felet och se vilken operation som misslyckades.
 
 // Modifierad Kod med Förbättrad Felhantering:
+// userIds ändrade vi och la in ett id i den array som troligtvis kommer rejectas
+
+// Steg 5: Hantera Fel i Individuella Promises
+// Om vi vill att de andra Promises ska fortsätta även om en av dem misslyckas, kan vi modifiera våra funktioner för att alltid resolve:a, men inkludera felinformation i resultatet.
 
 // Modifierade Funktioner:
+// function getUsersSafe(userId) {
+//   return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Kunde inte hämta användare med ID:et: ", userId);
+//       }
+//       return response.json();
+//     })
+//     .catch(() => {
+//       return { error: `Nätverksfel vid hämtning av ID: ${userId}` };
+//     });
+// }
 
-//   Modifierad Användning av Promise.all():
+// function getPostsSafe(userId) {
+//   return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(
+//           "Kunde inte hämta inlägg för användare med ID:et: ",
+//           userId
+//         );
+//       }
+//       return response.json();
+//     })
+//     .catch(() => {
+//       return {
+//         error: `Nätverksfel vid hämtning av inlägg för användare med ID: ${userId}`,
+//       };
+//     });
+// }
+
+// //   Modifierad Användning av Promise.all():
+// const userIds = [1, 2, 3, 9999999999];
+
+// const userPromises = userIds.map((id) => getUsersSafe(id));
+// const postPromises = userIds.map((id) => getPostsSafe(id));
+
+// Promise.all(userPromises).then((users) => {
+//   console.log("Användare hämtade: ", users);
+
+//   users.forEach((user) => {
+//     if (user.error) {
+//       console.error(user.error);
+//     } else {
+//       console.log("Användare hämtade: ", user.name);
+//     }
+//   });
+
+//   return Promise.all(postPromises);
+// });
 
 // Steg 6: Diskutera Fördelar och Nackdelar
 // Diskussionspunkter:
@@ -219,4 +271,114 @@ Promise.all(userPromises)
 // Alternativ:
 // Promise.allSettled(): Returnerar en Promise som resolve:ar efter att alla givna Promises har antingen resolve:at eller reject:at, med en array av resultat.
 // Promise.race(): Returnerar en Promise som resolve:ar eller reject:ar så snart som en av de givna Promises gör det.
+// -----------------------------------------------------
+// -----------------------------------------------------
+// -----------------------------------------------------
+// Torsdag: Introduktion till Async/Await
+
+// Grundläggande användning av async/await
+
+// En funktion som returnerar en Promise
+// function fetchData() {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve('Data hämtad!');
+//       }, 1000);
+//     });
+//   }
+
+// Använda async/await:
+
+// -----------------------------------------------------
+// Hur Async/Await Bygger på Promises
+
+// Kodexempel: Promise-kedja
+// function firstTask() {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         console.log('Första uppgiften klar.');
+//         resolve('Resultat från första uppgiften');
+//       }, 1000);
+//     });
+//   }
+
+//   function secondTask(data) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         console.log('Andra uppgiften klar med data:', data);
+//         resolve('Resultat från andra uppgiften');
+//       }, 1000);
+//     });
+//   }
+
+//   firstTask()
+//     .then(result1 => {
+//       return secondTask(result1);
+//     })
+//     .then(result2 => {
+//       console.log('Alla uppgifter klara med resultat:', result2);
+//     })
+//     .catch(error => {
+//       console.error('Ett fel uppstod:', error);
+//     });
+
+//     Konverterat till async/await:
+
+// -----------------------------------------------------
+// Konvertera Promise-kedjor till Async/Await
+
+// Steg-för-steg Guide:
+// Steg 1: Utgångspunkt - En Promise-baserad applikation
+// Vi utgår från koden som hämtar användare, deras inlägg och kommentarer med Promises.
+
+// Kodexempel:
+// function getUser(userId) {
+//     return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+//       .then(response => response.json());
+//   }
+
+//   function getPosts(userId) {
+//     return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+//       .then(response => response.json());
+//   }
+
+//   function getComments(postId) {
+//     return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+//       .then(response => response.json());
+//   }
+
+//   getUser(1)
+//     .then(user => {
+//       console.log('Användare:', user.name);
+//       return getPosts(user.id);
+//     })
+//     .then(posts => {
+//       const post = posts[0];
+//       console.log('Inlägg:', post.title);
+//       return getComments(post.id);
+//     })
+//     .then(comments => {
+//       console.log('Kommentarer till inlägget:');
+//       comments.forEach(comment => {
+//         console.log(`- ${comment.name}: ${comment.body}`);
+//       });
+//     })
+//     .catch(error => {
+//       console.error('Ett fel uppstod:', error);
+//     });
+
+//     Steg 2: Konvertera till Async/Await
+// Kodexempel:
+
+// -----------------------------------------------------
+// Steg 3: Diskutera Fördelar
+// Läsbarhet:
+// Koden ser mer ut som synkron kod, vilket underlättar förståelsen.
+
+// Felhantering:
+// Genom att använda try och catch kan vi hantera fel på ett mer intuitivt sätt.
+
+// Underhållbarhet:
+// Mindre risk för fel i komplexa kedjor av asynkrona operationer.
+
 // -----------------------------------------------------
---
+++ thursdayExercises.js
@@ -0,0 +1,42 @@
+// Praktiska Övningar
+// -----------------------------------------------------
+// Övning 1: Konvertera Promises till Async/Await (Lätt)
+
+// Uppgift:
+// Ta tidigare kod som använder Promises och skriv om den med async/await-syntax.
+
+// Instruktioner:
+// Använd koden från tidigare övningar eller code-alongs.
+// Fokusera på att använda try och catch för felhantering.
+
+// Mål:
+// Förstå hur async/await förenklar asynkron kod.
+// -----------------------------------------------------
+// Övning 2: Felhantering med Try/Catch (Medel)
+
+// Uppgift:
+// Skriv en async-funktion som hämtar data från ett API.
+// Implementera felhantering i asynkrona funktioner genom att använda try och catch runt await-anrop.
+
+// Instruktioner:
+// Simulera ett fel genom att använda en felaktig URL eller kasta ett fel om svaret inte är OK.
+// Visa användaren ett lämpligt felmeddelande.
+
+// Mål:
+// Lära sig att hantera fel i async/await-funktioner.
+// -----------------------------------------------------
+// Övning 3: Sekventiella och Parallella Asynkrona Operationer (Svår)
+
+// Uppgift:
+// Skriv en async-funktion som utför flera asynkrona operationer.
+// Utför först några operationer sekventiellt med await.
+// Utför sedan flera operationer parallellt med Promise.all() tillsammans med async-funktioner.
+
+// Instruktioner:
+// Hämta data från flera API-endpoints.
+// Använd Promise.all() för att köra vissa hämtningar parallellt.
+// Hantera fel som kan uppstå i någon av operationerna.
+
+// Mål:
+// Förstå hur man hanterar asynkrona operationer effektivt med async/await.
+// -----------------------------------------------------
